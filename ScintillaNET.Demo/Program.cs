47fae5f1843ed2b3d6ea82e8d232d14ebdf92fa8
fred <domaine75@gmail.com>
2023-04-29 15:45:12 +0200

    Clean code

--- ScintillaNET.Demo/Program.cs
+++ ScintillaNET.Demo/Program.cs
@@ -1,18 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Windows.Forms;
 
-namespace ScintillaNET.Demo {
-	static class Program {
-		/// <summary>
-		/// The main entry point for the application.
-		/// </summary>
-		[STAThread]
-		static void Main() {
-			Application.EnableVisualStyles();
-			Application.SetCompatibleTextRenderingDefault(false);
-			Application.Run(new MainForm());
-		}
-	}
+namespace ScintillaNET.Demo
+{
+  static class Program
+  {
+    /// <summary>
+    /// The main entry point for the application.
+    /// </summary>
+    [STAThread]
+    static void Main()
+    {
+      Application.EnableVisualStyles();
+      Application.SetCompatibleTextRenderingDefault(false);
+      Application.Run(new MainForm());
+    }
+  }
 }
